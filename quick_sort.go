5f88af78d30b5f2e8232bb27042a00e9e0958d84
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2024-03-19 21:16:18 +0700

    quick sort

--- quick_sort.go
+++ quick_sort.go
@@ -1,1 +1,30 @@
 package main
+
+import "fmt"
+
+func quickSort(list []int) []int {
+	var result []int
+	if len(list) < 2 {
+		return list
+	} else {
+		var pivot = list[0]
+		var less, greater []int
+		for i, v := range list {
+			if i != 0 {
+				if v <= pivot {
+					less = append(less, v)
+				} else {
+					greater = append(greater, v)
+				}
+			}
+		}
+		result = append(result, less...)
+		result = append(result, pivot)
+		result = append(result, greater...)
+		return result
+	}
+}
+
+func main() {
+	fmt.Println(quickSort([]int{3, 6, 1, 7, 9}))
+}
